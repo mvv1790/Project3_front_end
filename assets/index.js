00a7c0c8bd5acef4e041fd5ba8fc7936d6732bc2
mvv1790 <mvv1790@gmail.com>
2022-12-09 13:31:01 +0000

    Re-writing of the code

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,6 @@
-const playerCoice = prompt("Make your choice: ");
-const playerInput = prompt("You play: ")
-const computerCoice = alert("Computer plays: ");
+let round = 0
+let playerScore = 0 
+let computerScore = 0
 
-function storeInput(playerCoice) {
-    var answer = " ";
-    switch(val) {
-        case 1:
-            answer = "rock";
-            break;
-        case 2:
-            answer = "paper";
-            break;
-        case 3:
-            answer = "scissors";
-            break;
-    }
-    return answer
-}
+const choices = ["rock", "paper", "scissors"]
+
