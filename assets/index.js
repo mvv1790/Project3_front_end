449083d887fb1f236bd535418413a9080dfa3a25
mvv1790 <mvv1790@gmail.com>
2022-12-12 00:52:28 +0000

    Added game logic

--- assets/index.js
+++ assets/index.js
@@ -10,7 +10,7 @@ function gameState() {
   round = 1
 }
 
-function computerTurn() {return turn[Math.floor(Math.random) * 3]}
+function computerTurn() {return[Math.floor(Math.random) * 3]}
 
 function playerTurn(){
   let player = prompt(`Round ${round}: Make your choice, Rock, Paper or Scissors`)
@@ -23,6 +23,52 @@ function playerTurn(){
     console.log("Invalid input. Please try again.")
     player = playerTurn()
   }
-
   return player
 }
+
+function checkWinner() {
+  if (playerScore > computerScore) {
+    gameState();
+    return "Player wins the game!";
+  } else if (computerScore > playerScore) {
+    gameState();
+    return "Computer wins the game";
+  } else {
+    gameState();
+    return "The game is a draw!";
+  }
+}
+
+function playGame(player, computer) {
+  if (
+    (player === "rock" && computer === "scissors") || (player === "paper" && computer === "rock") || (player === "scissors" && computer === "paper")
+  ) {
+    playerScore = playerScore + 1;
+    console.log(`Player wins Round ${round}`);
+  } else if (
+    (player === "rock" && computer === "rock") || (player === "paper" && computer === "paper") || (player === "scissors" && computer === "scissors")
+  ) {
+    console.log(`Round ${round} is a draw`);
+  } else {
+    computerScore = computerScore + 1;
+    console.log(`Computer wins Round ${round}`);
+  }
+}
+
+function startGame(){
+  for (round; round < 6; round++) {
+    const computerSelection = computerTurn();
+    const player = playerTurn();
+
+    if (player === null) {
+      console.log("Game exited.");
+      gameState();
+      return;
+    }
+    playGame(player, computerSelection);
+  }
+  return console.log(checkWinner());
+}
+console.log(
+  "Welcome to Rock, Paper, Scissors. Please start the game by typing game() in the console"
+);
