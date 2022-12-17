3d6c6614dc5778c7c5e42f91b588521e3daf416b
mvv1790 <mvv1790@gmail.com>
2022-12-17 16:31:44 +0000

    Deploying bug workaround

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,9 @@
-let round = 1;
-let playerScore = 0;
-let computerScore = 0;
+let round = 1
+let playerScore = 0
+let computerScore = 0
 let computerChoice
 
-const choices = ["rock", "paper", "scissors"];
+const choices = ["rock", "paper", "scissors"]
 
 function computerPlay() {
   return choices[Math.floor(Math.random() * 3) + 1] 
@@ -24,33 +24,38 @@ function playerTurn() {
     `Round ${round}: Please type either Rock, Paper or Scissors:`
   );
 
-  if (!player) return null;
+  if (!player) return null
 
-  player = player.toLowerCase().trim();
+  player = player.toLowerCase().trim()
+
+  if (player === undefined && player === null) {
+    console.log("Wrong input, please try again.")
+    playerTurn()
+  }
 
   while (choices.indexOf(player) === -1) {
-    console.log("Wrong input, please try again.");
-    player = playerTurn();
+    console.log("Wrong input, please try again.")
+    player = playerTurn()
   }
-  return player;
+  return player
 }
 
 function gameState() {
-  playerScore = 0;
-  computerScore = 0;
-  round = 1;
+  playerScore = 0
+  computerScore = 0
+  round = 1
 }
 
 function checkWinner() {
   if (playerScore > computerScore) {
-    gameState();
-    return "Player wins.";
-  } else if (playerScore < computerScore) {
-    gameState();
-    return "Computer wins.";
+    gameState()
+    return "Player wins."
+  } else if (computerScore > playerScore) {
+    gameState()
+    return "Computer wins."
   } else {
-    gameState();
-    return "It's a draw!";
+    gameState()
+    return "It's a draw!"
   }
 }
 
@@ -60,37 +65,50 @@ function play(player, computer) {
     (player === "paper" && computer === "rock") ||
     (player === "scissors" && computer === "paper")
   ) {
-    playerScore = playerScore + 1;
-    console.log(`Player wins round ${round}.`);
+    playerScore = playerScore + 1
+    console.log(`Player wins round ${round}.`)
   } else if (
     (player === "scissors" && computer === "rock") ||
     (player === "rock" && computer === "paper") ||
     (player === "paper" && computer === "scissors")
   ) {
-    console.log(`Computer wins round ${round}.`);
+    computerScore = computerScore + 1
+    console.log(`Computer wins round ${round}.`)
   } else {
-    computerScore = computerScore + 1;
-    console.log(`Round ${round} is a draw.`);
+    console.log(`Round ${round} is a draw.`)
+  } 
+}
+
+function terminateGame() {
+  const player = playerTurn()
+  if (player !== null) {
+    
+    playerTurn()
+    return
   }
+  console.log("Terminated")
+  gameState()
+  return
 }
 
 function startGame() {
   firstMessage()
   for (round; round < 6; round++) {
-    const computerTurn = computerPlay();
-    const player = playerTurn();
+    const computerTurn = computerPlay()
+    const player = playerTurn()
 
-    if (player === null) {
-      console.log("Game exited.");
-      gameState();
-      return;
+    if (player === null || player === undefined) {
+      alert("Empty input will result in termination of the application.")
+      terminateGame()
+      alert("No input detected, application terminated.")
+      return
     }
 
-    play(player, computerTurn);
+    play(player, computerTurn)
     
 
   }
-  alert(checkWinner());
+  alert(checkWinner())
   lastMessage()
 }
 alert("To begin the game please input startGame() to the console")
